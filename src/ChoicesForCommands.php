18fafe0207b1f92136f0f7cf6123511b34997855
lazerg <lazerg2@gmail.com>
2022-03-02 10:21:29 +0500

    askWithChoice with null options;

--- src/ChoicesForCommands.php
+++ src/ChoicesForCommands.php
@@ -2,17 +2,24 @@
 
 namespace Lazerg\LaravelChoices;
 
+use Illuminate\Console\Command;
+
 trait ChoicesForCommands
 {
     /**
      * Ask with choices
      *
-     * @param array|string $options
+     * @param array|string|null $options
      * @param callable|null $action
      * @return Choice
      */
-    public function askWithChoices(array|string $options, callable $action = null): Choice
+    public function askWithChoices(array|string|null $options = null, ?callable $action = null): Choice
     {
+        /** @type Command $this */
+        if ($options === null) {
+            return new Choice($this);
+        }
+
         return (new Choice($this))->addChoice($options, $action);
     }
 }
--- src/ChoicesForSeeders.php
+++ src/ChoicesForSeeders.php
@@ -2,17 +2,24 @@
 
 namespace Lazerg\LaravelChoices;
 
+use Illuminate\Database\Seeder;
+
 trait ChoicesForSeeders
 {
     /**
      * Ask with choices
      *
-     * @param array|string $options
+     * @param array|string|null $options
      * @param callable|null $action
      * @return Choice
      */
-    public function askWithChoices(array|string $options, callable $action = null): Choice
+    public function askWithChoices(array|string|null $options = null, callable $action = null): Choice
     {
+        /** @type Seeder $this */
+        if ($options === null) {
+            return new Choice($this->command);
+        }
+
         return (new Choice($this->command))->addChoice($options, $action);
     }
 }
